5612fef1a7297667cfc8ad423cb2dbaa15041783
Brandon <brandonkindrick88@gmail.com>
2018-12-06 22:25:17 -0700

    Basic Node JS server
    Node JS server using Express and Socket.io

--- Tech Demo/app.js
+++ Tech Demo/app.js
@@ -1,1 +1,21 @@
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client', express.static(__dirname + '/client'));
+
+var port = 8080;
+serv.listen(port);
+console.log("Server is running on port: " + port);
+
+var io = require('socket.io')(serv, {});
+io.sockets.on('connection', function(socket){
+    console.log("New Connection");
+
+    socket.on('happy', function(data){
+        console.log(data.reason);
+    });
+});
