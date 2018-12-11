c0c0262f3fc36b411eed8b65d56a3c9d5e7f122f
Brandon <brandonkindrick88@gmail.com>
2018-12-10 22:24:41 -0700

    Node Server Communication
    Can take in commands from client and pass them to Command Line Program

--- Tech Demo/app.js
+++ Tech Demo/app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+var path = require("path");
+var fs = require("fs");
 var serv = require('http').Server(app);
+const spawn = require("child_process").spawn;
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/client/index.html');
@@ -18,4 +21,33 @@ io.sockets.on('connection', function(socket){
     socket.on('happy', function(data){
         console.log(data.reason);
     });
+
+    socket.on('add', function(data){
+        console.log(data.number1);
+        console.log(data.number2);
+        var answer = data.number1 + data.number2;
+        console.log(answer);
+       /* 
+        const pythonProcess = spawn('python', ['is.py', data.number1, data.number2]);
+        pythonProcess.stdout.on('data', (data) => {
+            console.log("hello");
+        });
+        */
+        socket.emit('serverMsg', {
+            msg: answer
+        });
+    });
+});
+
+const multer = require("multer");
+
+const handleError = (err, res) => {
+      res
+        .status(500)
+        .contentType("text/plain")
+        .end("Oops! Something went wrong!");
+};
+
+const upload = multer({
+    dest: "Capstone_Repo/uploads"
 });
