ce007a644749604058760daa75412f50055faa06
ChaddFrasier <cmf339@nau.edu>
2019-03-10 21:58:19 -0700

    ISIS integration has been achieved
    'miniconda3 isis3' must be installed in the host pc directory (miniconda3 = python 3.6)
    $USER_HOME/mininconda3/envs/isis3

--- TechDemo/ISSS.py
+++ TechDemo/ISSS.py
@@ -1,26 +1,32 @@
 from flask import Flask, render_template, request, url_for, Response, send_file
-from mdUtility import extractImage, cleanDirs, trimData, camptInterface, catoriglabInterface, catlabInterface, removeNulls
+from mdUtility import DataObject
 import os
 import ast
 from werkzeug.utils import secure_filename
-from subprocess import CalledProcessError
 
 
 # global variable
 # CURRENT_FILE will be removed later
-CURRENT_FILE = ''
-CURRENT_TPL_FILE = ''
+
 SAMPLE_LINES = 200
+
+# temp vars
+users = 0
+
 
 # Flask App Environment
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/uploads')
 IMAGE_FOLDER = os.path.join(APP_ROOT, 'static/images')
 PVL_FOLDER = os.path.join(APP_ROOT, 'static/pvl')
 TPL_FOLDER = os.path.join(APP_ROOT, 'static/tpl')
+CONFIG_FOLDER = os.path.join(APP_ROOT, 'static/config')
 
 app = Flask(__name__)
+app.secret_key = b'_5#7ey'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['CONFIG_FOLDER'] = CONFIG_FOLDER
 app.config['IMAGE_FOLDER'] = IMAGE_FOLDER
 app.config['PVL_FOLDER'] = PVL_FOLDER
 app.config['TPL_FOLDER'] = TPL_FOLDER
@@ -29,75 +35,101 @@ app.config['TPL_FOLDER'] = TPL_FOLDER
 # index.html
 @app.route("/", methods=["GET"])
 def index():
-    global CURRENT_FILE, CURRENT_TPL_FILE
-    CURRENT_FILE, CURRENT_TPL_FILE = "", ""
     return render_template('index.html')
 
 
 # upload and save process
 @app.route("/upload", methods=['GET', 'POST'])
 def upload():
+    global users
     # if request is post
     if request.method == 'POST':
+        # make file an instance of a data class and extract all the tags from that data.
+        # in order to do this i need a class called filedata
         # try to extract the file in the uploadFile form
         try:
-            # get cube file
             cubeFile = request.files['uploadFile']
-            # get template file
-            templateFile = request.files['templateFile']
+
         except KeyError:
             # catches NULL error and makes user try again
             print("Null File Error: please enter a .cub file ")
             return render_template("index.html")
 
-        if (templateFile.filename.split('.')[-1] == 'tpl' and templateFile.filename != '') \
-                and (cubeFile.filename.split('.')[-1] == 'cub' and cubeFile.filename != ''):
+        try:
+            tplFile = request.files['templateFile']
+            current_instance = DataObject(cubeFile.filename, tplFile.filename)
 
+        except KeyError:
+            current_instance = DataObject(cubeFile.filename)
+
+
+
+            # init div dict
+            current_instance.divDict = DataObject.initDict(current_instance)
+            # print(current_instance.divDict)
+
+
+        if (current_instance.tplFile.split('.')[-1] == 'tpl' and current_instance.tplFile != '') \
+                and (current_instance.filename.split('.')[-1] == 'cub' and current_instance.filename != ''):
             # save the file to the upload directory
-            cubeFile.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(cubeFile.filename)))
-            # save the current filepath to the global variable
-            global CURRENT_FILE
-            CURRENT_FILE = cubeFile.filename
+            cubeFile.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(current_instance.filename)))
+            if current_instance.tplFile != 'Default.tpl':
+                # save the file to the upload directory
+                tplFile.save(os.path.join(app.config['TPL_FOLDER'], secure_filename(current_instance.tplFile)))
 
-            # save the file to the upload directory
-            templateFile.save(os.path.join(app.config['TPL_FOLDER'], secure_filename(templateFile.filename)))
+            #   *--temp variable counter --*
+            users += 1
 
-            # save the current filepath to the global variable
-            global CURRENT_TPL_FILE
-            CURRENT_TPL_FILE = templateFile.filename
+            print(current_instance.filename)
+            print("total upload requests = " + str(users))
 
-        # check if the file is empty or not a cube
-        if CURRENT_FILE.split('.')[-1] == 'cub' and CURRENT_FILE != '':
             # this try except allows us to run a console command and catch any errors to stop from program crash
             try:
                 # this line is calling a string shell command by creating the command string on a single line
-                imagename = extractImage(CURRENT_FILE)
-                command_return_file = "return.pvl"
-                cleanDirs(app.config['PVL_FOLDER'], "ers", command_return_file)
-
-                # campt os call to terminal and saves in return file
-                camptInterface(CURRENT_FILE, command_return_file)
-                # catlab os call to terminal and saves in return file
-                catlabInterface(CURRENT_FILE, command_return_file)
-                # catoriglab os call to terminal and saves in return file
-                catoriglabInterface(CURRENT_FILE, command_return_file)
+                imagename = DataObject.extractImage(current_instance, current_instance.filename)
 
             except Exception as e:
-                  print(str(e))
+                print(str(e))
+
+            command_return_file = "return.pvl"
+            DataObject.cleanDirs(current_instance, app.config['PVL_FOLDER'], "ers", command_return_file)
 
             try:
-                templateFile = open(os.path.join(app.config['TPL_FOLDER'], CURRENT_TPL_FILE), "r")
+                # campt os call to terminal and saves in return file
+                DataObject.camptInterface(current_instance, current_instance.filename, command_return_file)
+            except Exception as e:
+                print(str(e))
+
+            try:
+                # catlab os call to terminal and saves in return file
+                DataObject.catlabInterface(current_instance, current_instance.filename, command_return_file)
+            except Exception as e:
+                print(str(e))
+
+            try:
+                # catoriglab os call to terminal and saves in return file
+                DataObject.catoriglabInterface(current_instance, current_instance.filename, command_return_file)
+            except Exception as e:
+                print(str(e))
+
+            try:
+                current_instance.rawFileData = DataObject.extractRawData(current_instance, os.path.join(app.config['PVL_FOLDER'], command_return_file))
+                print(current_instance.rawFileData)
+
+                templateFile = open(os.path.join(app.config['TPL_FOLDER'], current_instance.tplFile), "r")
                 templateString = templateFile.read()
                 templateFile.close()
+
                 full_filename = url_for('static', filename='images/' + imagename)
 
                 # parse file and fill dict
-                cubeDictionary = trimData(os.path.join(app.config['PVL_FOLDER'], command_return_file))
-                cubeDictionary['image'] = full_filename
+                current_instance.divDict = DataObject.trimData(current_instance, os.path.join(app.config['PVL_FOLDER'], command_return_file))
+                current_instance.divDict['image'] = full_filename
 
-                cubeDictionary = removeNulls(cubeDictionary)
+                current_instance.divDict = DataObject.removeNulls(current_instance, current_instance.divDict)
                 # return filled in text file to user
-                return render_template("output.html", DICTSTRING=cubeDictionary, TEMPAREA=templateString, IMG=cubeDictionary['image'])
+                return render_template("output.html", DICTSTRING=current_instance.divDict, TEMPAREA=templateString,
+                                       IMG=current_instance.divDict['image'])
             # catch file not found
             except FileNotFoundError:
                 print("ISIS3 command failed to create a pvl")
@@ -123,7 +155,7 @@ def getCSV():
             new_str,
             mimetype="text/csv",
             headers={"Content-disposition":
-                         "attachment; filename=exportISIS.csv"})
+                        "attachment; filename=exportISIS.csv"})
 
 
 @app.route('/getImage', methods=['GET', 'POST'])
@@ -138,8 +170,7 @@ def getImage():
                              attachment_filename=imagepath.split("/")[-1])
 
         except Exception as e:
-            return(str(e))
-
+            return str(e)
 
 
 # needed to run on command line
--- TechDemo/mdUtility.py
+++ TechDemo/mdUtility.py
@@ -8,7 +8,7 @@ Description:
     will also be home to all the isis3 command line interface functions for the various isis3 programs such as campt,
     catlab, isis2std, and catoriglab
 
-    This file is for the Caption Writing Project at USGS
+    This file is for the Caption Writing Project for NAU sponsored by USGS
 """
 from flask import Flask, render_template
 from subprocess import CalledProcessError
@@ -20,113 +20,149 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/uploads')
 IMAGE_FOLDER = os.path.join(APP_ROOT, 'static/images')
 PVL_FOLDER = os.path.join(APP_ROOT, 'static/pvl')
+CONFIG_FOLDER = os.path.join(APP_ROOT, 'static/config')
 TPL_FOLDER = os.path.join(APP_ROOT, 'static/tpl')
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['CONFIG_FOLDER'] = CONFIG_FOLDER
 app.config['IMAGE_FOLDER'] = IMAGE_FOLDER
 app.config['PVL_FOLDER'] = PVL_FOLDER
 app.config['TPL_FOLDER'] = TPL_FOLDER
 
 
-isis3md_dict = dict(TargetName='', SpacecraftName='', InstrumentId='', PlanetocentricLatitude='', ProjectionName='',
-                    StartTime='', SlantDistance='', SampleResolution='', NorthAzimuth='', Incidence='', Emission='',
-                    Phase='', SubSpacecraftGroundAzimuth='', SubSolarAzimuth='', MinimumLatitude='', MaximumLatitude='',
-                    MinimumLongitude='', MaximumLongitude='', image='')
+class DataObject:
+    # configuration dictionary
+    isis3md_dict = dict()
+
+    # construct using a filename
+    def __init__(self, filename, tplFile='Default.tpl', rawFileData=dict(), divDict=dict()):
+        self.filename = filename
+        self.rawFileData = rawFileData
+        self.divDict = divDict
+        self.tplFile = tplFile
+
+    def initDict(self, configFile='config1.cnf'):
+        file = open(os.path.join(app.config['CONFIG_FOLDER'],configFile), "r")
+        for line in file:
+            if '<tag>' in line:
+                self.divDict[line.split('<tag>')[1].split("</tag>")[0]] = ''
+
+        file.close()
+        return self.divDict
 
 
-# run isis3 command campt on a given file and save data into a large combined text file
-def camptInterface(cube, returnFile):
-    try:
-        os.system("campt from=" + os.path.join(app.config['UPLOAD_FOLDER'], cube)
-              + " to=" + os.path.join(app.config['PVL_FOLDER'], returnFile))
-    except IOError:
-        print("campt function failed")
+    # run isis3 command campt on a given file and save data into a large combined text file
+    def camptInterface(self, cube, returnFile):
+        try:
+            os.system("campt from=" + os.path.join(app.config['UPLOAD_FOLDER'], cube)
+                      + " to=" + os.path.join(app.config['PVL_FOLDER'], returnFile) + " append=True")
+        except IOError:
+            print("campt function failed")
+
+    def catlabInterface(self, cube, returnFile):
+        try:
+            os.system("catlab from=" + os.path.join(app.config['UPLOAD_FOLDER'], cube)
+                      + " to=" + os.path.join(app.config['PVL_FOLDER'], returnFile) + " append=True")
+        except IOError:
+            print("catlab function failed")
+
+    def catoriglabInterface(self, cube, returnFile):
+        try:
+            os.system("catoriglab from=" + os.path.join(app.config['UPLOAD_FOLDER'], cube)
+                      + " to=" + os.path.join(app.config['PVL_FOLDER'], returnFile) + " append=True")
+        except IOError:
+            print("catoriglab function failed")
+
+    # extract the image from a cube file to a specified format using isis2std() from the isis3 environment
+    # input -> cube file, format= png
+    # output -> name of image file that can be found in the static/images folder
+    def extractImage(self, cube, format='png'):
+        image = str(cube).split("cub")[0] + format
+        try:
+            os.system("isis2std from=" + os.path.join(app.config['UPLOAD_FOLDER'], str(cube)) + " to="
+                      + os.path.join(app.config['IMAGE_FOLDER'], image) + " format= " + format)
+        except CalledProcessError:
+            return CalledProcessError
+        return image
+
+    # takes a large string of data and cuts the
+    # string down to only the needed metadata strings
+    # this is to help save data into a structure later
+    # input -> string data
+    # output -> less string data
+    def trimData(self, file):
+        # right here use the dictionary that is created from the config file
+
+        mdfile = open(file, "r")
+        for line in mdfile:
+            if str(line).split('=')[0].strip(' ') in self.divDict.keys():
+                self.divDict[str(line).split('=')[0].strip(' ')] = str(line).split('=')[1].strip(' ').strip("\n")
+        mdfile.close()
+        return self.divDict
+
+    # extract a specific isis3 keyword from the data
+    # input -> keyword string, file to search in
+    # returns value of the keyword if found
+    # throws KeyError otherwise
+    #
+    # (FUNCTION MUST BE CAUGHT)
+    def extractIsisKeyword(self, cube, keyword):
+        try:
+
+            return os.system("getkey from= " + os.path.join(app.config['UPLOAD_FOLDER'], str(cube))
+                             + " keyword=" + str(keyword) + " recursive= True")
+        except Exception as e:
+            print(str(e))
+
+    # will be called after the user hits restart or when some files are no longer needed
+    # flush working directories of unnecessary files for performance reasons
+    # function:
+    #     inputs-> directory string, code to tell the function what to do to the directory, and optional file variable
+    #
+    #     CLEAR all files ; code = clr and file == NULL
+    #     CLEAR all file of type file ; code= clr and file != NULL
+    #     DELETE file when code = del and file != NULL
+    #     ERASE contents of a file; code= ers file != NULL
+    #
+    def cleanDirs(self, directory, code, file=''):
+        # erase contents of any file given a directory and the filename
+        try:
+            if code == 'ers' and file != '':
+                if file != '':
+                    open(os.path.join(directory, file), "w").close()
+                return 0
+        except FileNotFoundError:
+            return render_template("error.html")
+
+    def removeNulls(self, dict):
+        for key in dict:
+            if dict.get(key) != "":
+                self.divDict[key] = dict[key]
+            else:
+                self.divDict[key] = None
+
+        return self.divDict
+
+    # extract all raw data from cube file
+    def extractRawData(self, pvlFile):
+        file = open(pvlFile, "r", encoding="utf-8")
+
+        for line in file:
+            line = line.strip().split('\n')[0]
+            # print(line.strip().split("="))
+            if len(line.split("=")) > 1 and '(' not in line.split('=')[1] and\
+                    'Group' not in line.split("=")[0].strip() and 'Object' not in line.split("=")[0].strip():
+                self.rawFileData[str(line.split("=")[0].strip())] = line.split("=")[-1].strip()
+
+        file.close()
+        return self.rawFileData
 
 
-def catlabInterface(cube, returnFile):
-    try:
-        os.system("catlab from=" + os.path.join(app.config['UPLOAD_FOLDER'], cube)
-              + " to=" + os.path.join(app.config['PVL_FOLDER'], returnFile))
-    except IOError:
-        print("catlab function failed")
-
-def catoriglabInterface(cube, returnFile):
-    try:
-        os.system("catoriglab from=" + os.path.join(app.config['UPLOAD_FOLDER'], cube)
-                  + " to=" + os.path.join(app.config['PVL_FOLDER'], returnFile))
-    except IOError:
-        print("catoriglab function failed")
 
 
-# extract the image from a cube file to a specified format using isis2std() from the isis3 environment
-# input -> cube file, format= png
-# output -> name of image file that can be found in the static/images folder
-def extractImage(cube, format='png'):
-    image = str(cube).split("cub")[0] + format
-    try:
-        os.system("isis2std from=" + os.path.join(app.config['UPLOAD_FOLDER'], str(cube)) + " to="
-                  + os.path.join(app.config['IMAGE_FOLDER'], image) + " format= " + format)
-    except CalledProcessError:
-        return CalledProcessError
-    return image
 
 
-# takes a large string of data and cuts the
-# string down to only the needed metadata strings
-# this is to help save data into a structure later
-# input -> string data
-# output -> less string data
-def trimData(file):
-    trimmed_dict = isis3md_dict
-    mdfile = open(file, "r")
-    for line in mdfile:
-        if str(line).split('=')[0].strip(' ') in trimmed_dict.keys():
-            trimmed_dict[str(line).split('=')[0].strip(' ')] = str(line).split('=')[1].strip(' ').strip("\n")
-
-    mdfile.close()
-    return trimmed_dict
 
 
-# extract a specific isis3 keyword from the data
-# input -> keyword string, file to search in
-# returns value of the keyword if found
-# throws KeyError otherwise
-#
-# (FUNCTION MUST BE CAUGHT)
-def extractIsisKeyword(cube, keyword):
-    try:
 
-        return os.system("getkey from= " + os.path.join(app.config['UPLOAD_FOLDER'], str(cube))
-                     + " keyword=" + str(keyword) + " recursive= True")
-    except Exception as e:
-        print(str(e))
-
-# will be called after the user hits restart or when some files are no longer needed
-# flush working directories of unnecessary files for performance reasons
-# function:
-#     inputs-> directory string, code to tell the function what to do to the directory, and optional file variable
-#
-#     CLEAR all files ; code = clr and file == NULL
-#     CLEAR all file of type file ; code= clr and file != NULL
-#     DELETE file when code = del and file != NULL
-#     ERASE contents of a file; code= ers file != NULL
-#
-def cleanDirs(directory, code, file=''):
-    # erase contents of any file given a directory and the filename
-    try:
-        if code == 'ers' and file != '':
-            if file != '':
-                open(os.path.join(directory, file), "w").close()
-            return 0
-    except FileNotFoundError:
-        return render_template("error.html")
-
-
-def removeNulls(dict):
-    returnDict = {}
-    for key in dict:
-        if dict.get(key) != "":
-            returnDict[key] = dict[key]
-
-    return returnDict
